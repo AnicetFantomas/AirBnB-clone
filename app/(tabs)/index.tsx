0723baab5c53690bc21aaad82623c25d91960040
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-30 19:35:06 +0200

    Add login modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const Page = () => {
   return (
     <View>
-      <Text>Page</Text>
+      <Link href={"/(modals)/login"}>
+        Login
+      </Link>
+      <Link href={"/(modals)/booking"}>
+        Login
+      </Link>
+      <Link href={"/listing/1209"}>
+        Listing details 
+      </Link>
     </View>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,8 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -24,7 +19,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    'mon': require("../assets/fonts/Montserrat-Regular.ttf"),
+    mon: require("../assets/fonts/Montserrat-Regular.ttf"),
     "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
     "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
@@ -48,9 +43,24 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Log in or Sign up",
+          headerTitleStyle: { fontFamily: "mon-sb" },
+          presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          )
+        }}
+      />
     </Stack>
   );
 }
