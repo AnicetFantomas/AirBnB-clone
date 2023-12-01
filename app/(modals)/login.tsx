332c296044905352907fcf7235661853957bdaf5
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-01 10:27:00 +0200

    Add login styles inside constants

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
 
 const Page = () => {
+  useWarmUpBrowser();
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <TextInput autoCapitalize="none" placeholder="Email" style={} />
     </View>
-  )
-}
+  );
+};
 
-export default Page
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 26,
+  },
+});
+
+export default Page;
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 const Page = () => {
+  const { signOut, isSignedIn } = useAuth();
   return (
     <View>
-      <Text>Profile</Text>
+      <Button title="Log out" onPress={() => signOut()} />
+      {!isSignedIn && (
+        <Link href={"/(modals)/login"}>
+          <Text>Log in</Text>
+        </Link>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
