d6f0a25dc2079f69e3202b5c306df9338cfb89cc
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-01 11:19:09 +0200

    Update login styles inside constants

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,46 @@
-import { View, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import React from "react";
 import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
+import { defaultStyles } from "../../constants/styles";
+import Colors from "../../constants/Colors";
 
 const Page = () => {
   useWarmUpBrowser();
   return (
     <View style={styles.container}>
-      <TextInput autoCapitalize="none" placeholder="Email" style={} />
+      <TextInput
+        autoCapitalize="none"
+        placeholder="Email"
+        placeholderTextColor={"grey"}
+        style={[defaultStyles.inputField, { marginBottom: 30 }]}
+      />
+      <TouchableOpacity style={defaultStyles.btn}>
+        <Text style={defaultStyles.btnText}>Continue</Text>
+      </TouchableOpacity>
+
+      <View style={styles.separatorView}>
+        <View
+          style={{
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            flex: 1,
+          }}
+        />
+        <Text style={styles.separator}>or</Text>
+        <View
+          style={{
+            borderBottomColor: "#000",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            flex: 1,
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -16,6 +50,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     padding: 26,
+  },
+  separatorView: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 30
+  },
+  separator: {
+    fontFamily: "mon-sb",
+    color: Colors.grey,
   },
 });
 
