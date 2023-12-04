5c7492cf05f7b0a87af7e1697bb15ec0996f45e8
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-04 17:09:49 +0200

    Add search bar to the app

--- components/ExplorerHeader.tsx
+++ components/ExplorerHeader.tsx
@@ -38,10 +38,20 @@ const ExplorerHeader = () => {
     },
   ];
   return (
-    <SafeAreaView style={{ flex: 1,backgroundColor:'fff' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "fff" }}>
       <View style={styles.container}>
         <View style={styles.actionRow}>
-          <Link href={"/(modals)/booking"}>Booking</Link>
+          <Link href={"/(modals)/booking"} asChild>
+            <TouchableOpacity style={styles.searchBtn}>
+              <Ionicons name="search" size={24} />
+              <View>
+                <Text style={{ fontFamily: "mon-sb" }}>Where to?</Text>
+                <Text style={{ fontFamily: "mon", color: Colors.grey }}>
+                  Anywhere · any week
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </Link>
           <TouchableOpacity style={styles.filterBtn}>
             <Ionicons name="options-outline" size={24} />
           </TouchableOpacity>
@@ -59,16 +69,37 @@ const styles = StyleSheet.create({
   actionRow: {
     flexDirection: "row",
     alignContent: "center",
-    justifyContent: 'space-between',
-    paddingHorizontal:24,
-    paddingBottom:16
+    justifyContent: "space-between",
+    paddingHorizontal: 24,
+    paddingBottom: 16,
+    gap:10,
   },
   filterBtn: {
     padding: 10,
     borderWidth: 1,
     borderColor: Colors.grey,
-    borderRadius: 24
-  }
+    borderRadius: 24,
+  },
+  searchBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    borderColor: "#c2c2c2",
+    borderWidth: StyleSheet.hairlineWidth,
+    width:280,
+    flex:1,
+    borderRadius: 30,
+    backgroundColor:'#fff',
+    padding:14,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.12,
+    shadowRadius:8,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
 });
 
 export default ExplorerHeader;
