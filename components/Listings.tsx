459848408cb9d3049683504d7c797ac6f01fa1f5
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-05 11:17:46 +0200

    display room details on the page

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -5,12 +5,13 @@ import {
   FlatList,
   StyleSheet,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import React, { useEffect, useRef, useState } from "react";
 import { defaultStyles } from "../constants/styles";
 import { Link } from "expo-router";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { Listing } from "../interfaces/listing";
+import { Ionicons } from "@expo/vector-icons";
 
 interface Props {
   listings: any[];
@@ -34,6 +35,29 @@ const Listings = ({ listings: items, category }: Props) => {
       <TouchableOpacity>
         <View style={styles.listing}>
           <Image source={{ uri: item.medium_url }} style={styles.image} />
+          <TouchableOpacity
+            style={{ position: "absolute", right: 30, top: 30 }}
+          >
+            <Ionicons name="heart-outline" size={24} color={"#000"} />
+          </TouchableOpacity>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={{ fontFamily: "mon-sb", fontSize: 16 }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Ionicons name="star" size={16} />
+              <Text style={{ fontFamily: "mon-sb" }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+          <View style={{ flexDirection: "row", gap: 4 }}>
+            <Text style={{ fontFamily: "mon-sb" }}>$ {item.price}</Text>
+            <Text style={{ fontFamily: "mon-sb" }}>night</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </Link>
@@ -53,11 +77,14 @@ const Listings = ({ listings: items, category }: Props) => {
 const styles = StyleSheet.create({
   listing: {
     padding: 16,
+    gap: 10,
+    marginVertical: 16,
   },
   image: {
-    width: '100%', 
-    height: 300
-  }
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
 });
 
 export default Listings;
