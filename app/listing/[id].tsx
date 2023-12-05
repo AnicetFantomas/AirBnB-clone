78357cbfaf2a5ae060f9bccac05516e71198af34
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-05 14:00:55 +0200

    Add page item animations

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,46 +1,77 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
-import listingsData from '../../assets/data/airbnb-listings.json'
-import Animated from 'react-native-reanimated'
-import { Listing } from '../../interfaces/listing'
-import { Dimensions } from 'react-native'
-import Colors from '../../constants/Colors'
-import { Ionicons } from '@expo/vector-icons'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import listingsData from "../../assets/data/airbnb-listings.json";
+import Animated, { SlideInDown, interpolate, useAnimatedRef, useAnimatedStyle, useScrollViewOffset } from "react-native-reanimated";
+import { Listing } from "../../interfaces/listing";
+import { Dimensions } from "react-native";
+import Colors from "../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { defaultStyles } from "../../constants/styles";
 
-const IMG_HEIGHT = 300
-const {width} = Dimensions.get('window');
+const IMG_HEIGHT = 300;
+const { width } = Dimensions.get("window");
 
 const Page = () => {
-    const {id} =useLocalSearchParams<{id:string}>();
-    const listing: Listing = (listingsData as any[]).find((item) => item.id === id);
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const listing: Listing = (listingsData as any[]).find(
+    (item) => item.id === id
+  );
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollViewOffset(scrollRef);
+
+  const imageAnimatedStyle = useAnimatedStyle(() => {
+    return{
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value, 
+            [-IMG_HEIGHT, 0, IMG_HEIGHT],
+            [-IMG_HEIGHT/4, 0, IMG_HEIGHT * 0.75]
+          )
+        },
+        {
+          scale: interpolate(scrollOffset.value, [-IMG_HEIGHT, 0, IMG_HEIGHT], [2,1,1])
+        }
+      ]
+    }
+  });
 
   return (
     <View style={styles.container}>
-      <Animated.ScrollView>
-        <Animated.Image source={{uri: listing.xl_picture_url}} style={styles.image}/>
+      <Animated.ScrollView ref={scrollRef} contentContainerStyle={{paddingBottom: 100}} scrollEventThrottle={16}>
+        <Animated.Image
+          source={{ uri: listing.xl_picture_url }}
+          style={[styles.image, imageAnimatedStyle]}
+        />
         <View style={styles.infoContainer}>
           <Text style={styles.name}>{listing.name}</Text>
           <Text style={styles.location}>
             {listing.room_type} in {listing.smart_location}
           </Text>
           <Text style={styles.rooms}>
-            {listing.guests_included} guests · {listing.bedrooms} bedrooms · {listing.beds} bed ·{' '}
-            {listing.bathrooms} bathrooms
+            {listing.guests_included} guests · {listing.bedrooms} bedrooms ·{" "}
+            {listing.beds} bed · {listing.bathrooms} bathrooms
           </Text>
-          <View style={{ flexDirection: 'row', gap: 4 }}>
+          <View style={{ flexDirection: "row", gap: 4 }}>
             <Ionicons name="star" size={16} />
             <Text style={styles.ratings}>
-              {listing.review_scores_rating / 20} · {listing.number_of_reviews} reviews
+              {listing.review_scores_rating / 20} · {listing.number_of_reviews}{" "}
+              reviews
             </Text>
           </View>
           <View style={styles.divider} />
 
           <View style={styles.hostView}>
-            <Image source={{ uri: listing.host_picture_url }} style={styles.host} />
+            <Image
+              source={{ uri: listing.host_picture_url }}
+              style={styles.host}
+            />
 
             <View>
-              <Text style={{ fontWeight: '500', fontSize: 16 }}>Hosted by {listing.host_name}</Text>
+              <Text style={{ fontWeight: "500", fontSize: 16 }}>
+                Hosted by {listing.host_name}
+              </Text>
               <Text>Host since {listing.host_since}</Text>
             </View>
           </View>
@@ -50,14 +81,25 @@ const Page = () => {
           <Text style={styles.description}>{listing.description}</Text>
         </View>
       </Animated.ScrollView>
+      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <TouchableOpacity style={styles.footerText}>
+              <Text style={styles.footerPrice}>$ {listing.price}</Text>
+              <Text>night</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[defaultStyles.btn, {paddingHorizontal:20}]}>
+              <Text style={defaultStyles.btnText}>Reserve</Text>
+            </TouchableOpacity>
+          </View>
+      </Animated.View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   image: {
     height: IMG_HEIGHT,
@@ -65,27 +107,27 @@ const styles = StyleSheet.create({
   },
   infoContainer: {
     padding: 24,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   name: {
     fontSize: 26,
-    fontWeight: 'bold',
-    fontFamily: 'mon-sb',
+    fontWeight: "bold",
+    fontFamily: "mon-sb",
   },
   location: {
     fontSize: 18,
     marginTop: 10,
-    fontFamily: 'mon-sb',
+    fontFamily: "mon-sb",
   },
   rooms: {
     fontSize: 16,
     color: Colors.grey,
     marginVertical: 4,
-    fontFamily: 'mon',
+    fontFamily: "mon",
   },
   ratings: {
     fontSize: 16,
-    fontFamily: 'mon-sb',
+    fontFamily: "mon-sb",
   },
   divider: {
     height: StyleSheet.hairlineWidth,
@@ -99,38 +141,38 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.grey,
   },
   hostView: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 12,
   },
   footerText: {
-    height: '100%',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
+    height: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
     gap: 4,
   },
   footerPrice: {
     fontSize: 18,
-    fontFamily: 'mon-sb',
+    fontFamily: "mon-sb",
   },
   roundButton: {
     width: 40,
     height: 40,
     borderRadius: 50,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
     color: Colors.primary,
   },
   bar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 10,
   },
   header: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     height: 100,
     borderBottomWidth: StyleSheet.hairlineWidth,
     borderColor: Colors.grey,
@@ -139,8 +181,8 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     marginTop: 10,
-    fontFamily: 'mon',
+    fontFamily: "mon",
   },
-})
+});
 
-export default Page
+export default Page;
