d114fcebd575d98bdb0c0f2db00c97d0cb401270
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-05 11:53:11 +0200

    Add animation in the app

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -12,6 +12,7 @@ import { defaultStyles } from "../constants/styles";
 import { Link } from "expo-router";
 import { Listing } from "../interfaces/listing";
 import { Ionicons } from "@expo/vector-icons";
+import Animated, { FadeInRight, FadeOutLeft } from 'react-native-reanimated'
 
 interface Props {
   listings: any[];
@@ -27,13 +28,13 @@ const Listings = ({ listings: items, category }: Props) => {
     setLoanding(true);
     setTimeout(() => {
       setLoanding(false);
-    }, 2000);
+    }, 200);
   }, [category]);
 
   const renderRow: ListRenderItem<Listing> = ({ item }) => (
     <Link href={`/listing/${item.id}`} asChild>
       <TouchableOpacity>
-        <View style={styles.listing}>
+        <Animated.View style={styles.listing} entering={FadeInRight} exiting={FadeOutLeft}>
           <Image source={{ uri: item.medium_url }} style={styles.image} />
           <TouchableOpacity
             style={{ position: "absolute", right: 30, top: 30 }}
@@ -58,7 +59,7 @@ const Listings = ({ listings: items, category }: Props) => {
             <Text style={{ fontFamily: "mon-sb" }}>$ {item.price}</Text>
             <Text style={{ fontFamily: "mon-sb" }}>night</Text>
           </View>
-        </View>
+        </Animated.View>
       </TouchableOpacity>
     </Link>
   );
