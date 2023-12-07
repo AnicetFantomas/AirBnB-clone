9342cce538ea9e4b6a2b1f90888ccaee53348622
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-07 16:10:23 +0200

    Add cards

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,18 +1,59 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import {BlurView} from 'expo-blur'
 import Animated, { SlideInDown } from 'react-native-reanimated'
 import { defaultStyles } from '../../constants/styles'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import { router } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
+import Colors from '../../constants/Colors'
+
+const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
 
 const Page = () => {
-  const onClearAll = () => {};
+  const router = useRouter();
+const [openCard, setOpenCard] = useState(0);
+const [selectedPLace, setSelectedPLace] = useState(0);
+
+  const onClearAll = () => {
+    setSelectedPLace(0);
+    setOpenCard(0);
+  };
 
   return (
     <BlurView intensity={70} style={styles.container} tint='light'>
-      <Text>booking</Text>
+     <View style={styles.card} >
+        {
+          openCard != 0 && (
+            <AnimatedTouchableOpacity onPress={()=> setOpenCard(0)}>
+                <Text style={styles.previewText}>Where</Text>
+                <Text style={styles.previewDate}>I'm flexible</Text>
+            </AnimatedTouchableOpacity>
+          )
+        }
+     </View>
+
+     <View style={styles.card} >
+        {
+          openCard != 1 && (
+            <AnimatedTouchableOpacity onPress={()=> setOpenCard(1)}>
+                <Text style={styles.previewText}>When</Text>
+                <Text style={styles.previewDate}>Any week</Text>
+            </AnimatedTouchableOpacity>
+          )
+        }
+     </View>
+
+     <View style={styles.card} >
+        {
+          openCard != 2 && (
+            <AnimatedTouchableOpacity onPress={()=> setOpenCard(2)}>
+                <Text style={styles.previewText}>Who</Text>
+                <Text style={styles.previewDate}>Add guests</Text>
+            </AnimatedTouchableOpacity>
+          )
+        }
+     </View>
       <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
           <TouchableOpacity onPress={onClearAll} style={{justifyContent: 'center'}}>
@@ -32,6 +73,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 100
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius:14,
+    margin: 10,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.3,
+    shadowRadious: 4,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    gap: 20
+  },
+  previewText: {
+    fontFamily:'mon-sb',
+    fontSize: 14,
+    color: Colors.grey
+  },
+  previewDate: {
+    fontFamily:'mon-sb',
+    fontSize: 14,
+    color: Colors.dark
   }
 })
 
