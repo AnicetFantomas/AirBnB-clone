6ba57cf3492758b61528ac501a824bb2cfa70000
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-07 16:18:58 +0200

    Add cards animations

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,19 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
-import {BlurView} from 'expo-blur'
-import Animated, { SlideInDown } from 'react-native-reanimated'
-import { defaultStyles } from '../../constants/styles'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { useRouter } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons'
-import Colors from '../../constants/Colors'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { BlurView } from "expo-blur";
+import Animated, { FadeIn, FadeOut, SlideInDown } from "react-native-reanimated";
+import { defaultStyles } from "../../constants/styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../../constants/Colors";
 
-const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 const Page = () => {
   const router = useRouter();
-const [openCard, setOpenCard] = useState(0);
-const [selectedPLace, setSelectedPLace] = useState(0);
+  const [openCard, setOpenCard] = useState(0);
+  const [selectedPLace, setSelectedPLace] = useState(0);
 
   const onClearAll = () => {
     setSelectedPLace(0);
@@ -21,62 +22,99 @@ const [selectedPLace, setSelectedPLace] = useState(0);
   };
 
   return (
-    <BlurView intensity={70} style={styles.container} tint='light'>
-     <View style={styles.card} >
-        {
-          openCard != 0 && (
-            <AnimatedTouchableOpacity onPress={()=> setOpenCard(0)}>
-                <Text style={styles.previewText}>Where</Text>
-                <Text style={styles.previewDate}>I'm flexible</Text>
-            </AnimatedTouchableOpacity>
-          )
-        }
-     </View>
+    <BlurView intensity={70} style={styles.container} tint="light">
+      <View style={styles.card}>
+        {openCard != 0 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(0)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Where</Text>
+            <Text style={styles.previewDate}>I'm flexible</Text>
+          </AnimatedTouchableOpacity>
+        )}
+      </View>
 
-     <View style={styles.card} >
-        {
-          openCard != 1 && (
-            <AnimatedTouchableOpacity onPress={()=> setOpenCard(1)}>
-                <Text style={styles.previewText}>When</Text>
-                <Text style={styles.previewDate}>Any week</Text>
-            </AnimatedTouchableOpacity>
-          )
-        }
-     </View>
+      <View style={styles.card}>
+        {openCard != 1 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(1)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>When</Text>
+            <Text style={styles.previewDate}>Any week</Text>
+          </AnimatedTouchableOpacity>
+        )}
+      </View>
 
-     <View style={styles.card} >
-        {
-          openCard != 2 && (
-            <AnimatedTouchableOpacity onPress={()=> setOpenCard(2)}>
-                <Text style={styles.previewText}>Who</Text>
-                <Text style={styles.previewDate}>Add guests</Text>
-            </AnimatedTouchableOpacity>
-          )
-        }
-     </View>
-      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
-          <TouchableOpacity onPress={onClearAll} style={{justifyContent: 'center'}}>
-            <Text style={{fontSize: 18, fontFamily:'mon-sb', textDecorationLine: 'underline'}}>Clear All</Text>
+      <View style={styles.card}>
+        {openCard != 2 && (
+          <AnimatedTouchableOpacity
+            onPress={() => setOpenCard(2)}
+            style={styles.cardPreview}
+            entering={FadeIn.duration(200)}
+            exiting={FadeOut.duration(200)}
+          >
+            <Text style={styles.previewText}>Who</Text>
+            <Text style={styles.previewDate}>Add guests</Text>
+          </AnimatedTouchableOpacity>
+        )}
+      </View>
+      <Animated.View
+        style={defaultStyles.footer}
+        entering={SlideInDown.delay(200)}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={onClearAll}
+            style={{ justifyContent: "center" }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontFamily: "mon-sb",
+                textDecorationLine: "underline",
+              }}
+            >
+              Clear All
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.back()} style={[defaultStyles.btn, {paddingRight:20, paddingLeft:50}]}>
-            <Ionicons name='search-outline' size={24} color={'#fff'} style={defaultStyles.btnIcon} />
+          <TouchableOpacity
+            onPress={() => router.back()}
+            style={[defaultStyles.btn, { paddingRight: 20, paddingLeft: 50 }]}
+          >
+            <Ionicons
+              name="search-outline"
+              size={24}
+              color={"#fff"}
+              style={defaultStyles.btnIcon}
+            />
             <Text style={defaultStyles.btnText}>Search</Text>
           </TouchableOpacity>
         </View>
       </Animated.View>
     </BlurView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 100
+    paddingTop: 100,
   },
   card: {
-    backgroundColor: '#fff',
-    borderRadius:14,
+    backgroundColor: "#fff",
+    borderRadius: 14,
     margin: 10,
     elevation: 4,
     shadowColor: "#000",
@@ -86,18 +124,23 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
-    gap: 20
+    gap: 20,
   },
   previewText: {
-    fontFamily:'mon-sb',
+    fontFamily: "mon-sb",
     fontSize: 14,
-    color: Colors.grey
+    color: Colors.grey,
   },
   previewDate: {
-    fontFamily:'mon-sb',
+    fontFamily: "mon-sb",
     fontSize: 14,
-    color: Colors.dark
-  }
-})
+    color: Colors.dark,
+  },
+  cardPreview: {
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 20,
+  },
+});
 
-export default Page
+export default Page;
