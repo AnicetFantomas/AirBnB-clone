c0d408742def9aa3a20076fb8ae766d756276c3d
AnicetFantomas <anicetmurhula20@gmail.com>
2023-12-07 12:25:06 +0200

    Add blur style to the modal page

--- app/(modals)/booking.tsx
+++ app/(modals)/booking.tsx
@@ -1,11 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import {BlurView} from 'expo-blur'
+import Animated, { SlideInDown } from 'react-native-reanimated'
+import { defaultStyles } from '../../constants/styles'
 
 const Page = () => {
   return (
     <BlurView intensity={70} style={styles.container} tint='light'>
       <Text>booking</Text>
+      <Animated.View style={defaultStyles.footer} entering={SlideInDown.delay(200)}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
+
+        </View>
+      </Animated.View>
     </BlurView>
   )
 }
--- components/ModalHeaderText.tsx
+++ components/ModalHeaderText.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import Colors from "../constants/Colors";
 
 const ModalHeaderText = () => {
-  return (
-    <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-    </View>
-  )
-}
+  const [active, setActive] = useState(0);
 
-export default ModalHeaderText
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "center", gap: 10 }}>
+      <TouchableOpacity onPress={() => setActive(0)}>
+        <Text
+          style={{
+            fontFamily: "mon-sb",
+            fontSize: 18,
+            color: active === 0 ? "#000" : Colors.grey,
+            textDecorationLine: active === 0 ? "underline" : "none",
+          }}
+        >
+          Stays
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setActive(1)}>
+        <Text
+          style={{
+            fontFamily: "mon-sb",
+            fontSize: 18,
+            color: active === 1 ? "#000" : Colors.grey,
+            textDecorationLine: active === 1 ? "underline" : "none",
+          }}
+        >
+          Experiences
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ModalHeaderText;
